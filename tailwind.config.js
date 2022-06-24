db59ada6020157463b9575aa14ccb7e10e093169
Esoteric918 <3411@holbertonschool.com>
2022-06-24 13:48:26 -0500

    work in progress

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-to-t': 'linear-gradient(to top, #f8f8f8 0%, #e6e6e6 100%)',
+    },
+  },
+}
+  // plugins: [],
+}
