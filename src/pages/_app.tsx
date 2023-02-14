32f335a02807dbeeee7fe0aef09e2bb8c2834e4e
esoteric918 <3411@holbertonstudent.com>
2023-02-14 12:27:26 -0600

    move files to main repo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <Dashboard />
     <Component {...pageProps} />
     <Contact />
-    <Footer /></>
+    <Footer />
+    </>
     )
 }
